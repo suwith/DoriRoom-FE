e06c289bd16cc8a60cbb1908f75a0a008f4cf228
rktlskan021 <rktlskan021@gmail.com>
2025-06-27 16:24:44 +0900

    chore: eslint.config.mjs 설정 추가

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,42 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+import { FlatCompat } from '@eslint/eslintrc'
+import eslintPluginPrettier from 'eslint-plugin-prettier'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-});
+})
 
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
+const eslintConfig = [
+  ...compat.config({
+    extends: ['next/core-web-vitals', 'prettier'],
+    rules: {
+      // 기본 스타일 규칙
+      semi: ['error', 'never'],
+      quotes: ['error', 'single'],
+      indent: ['error', 2],
+      'no-console': 'warn',
 
-export default eslintConfig;
+      // camelCase: 변수, 함수
+      camelcase: ['error', { properties: 'always' }],
+
+      // PascalCase: 클래스/컴포넌트
+      'new-cap': ['error', { newIsCap: true, capIsNew: false }],
+    },
+  }),
+
+  // prettier 적용
+  {
+    plugins: {
+      prettier: eslintPluginPrettier,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+    },
+  },
+]
+
+export default eslintConfig
