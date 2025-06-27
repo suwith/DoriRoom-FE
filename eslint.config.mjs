bb88bf108343c0a33d1b933a1323a0b8eb0e3b93
rktlskan021 <rktlskan021@gmail.com>
2025-06-27 16:40:20 +0900

    chore: eslint 파일 수정
    console 사용 금지 주석
    함수 생성할 때 function 선언만 허용

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,30 +1,32 @@
-import { dirname } from 'path'
-import { fileURLToPath } from 'url'
-import { FlatCompat } from '@eslint/eslintrc'
-import eslintPluginPrettier from 'eslint-plugin-prettier'
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
+import eslintPluginPrettier from 'eslint-plugin-prettier';
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-})
+});
 
 const eslintConfig = [
   ...compat.config({
     extends: ['next/core-web-vitals', 'prettier'],
     rules: {
       // 기본 스타일 규칙
-      semi: ['error', 'never'],
       quotes: ['error', 'single'],
       indent: ['error', 2],
-      'no-console': 'warn',
+      // 'no-console': 'warn',
 
       // camelCase: 변수, 함수
       camelcase: ['error', { properties: 'always' }],
 
       // PascalCase: 클래스/컴포넌트
       'new-cap': ['error', { newIsCap: true, capIsNew: false }],
+
+      // 함수는 function 선언만 허용, 화살표 함수 금지
+      'func-style': ['error', 'declaration', { allowArrowFunctions: false }],
     },
   }),
 
@@ -37,6 +39,6 @@ const eslintConfig = [
       'prettier/prettier': 'error',
     },
   },
-]
+];
 
-export default eslintConfig
+export default eslintConfig;
