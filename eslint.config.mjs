29003705eb284373918bb8087e811b0c9bd20925
rktlskan021 <rktlskan021@gmail.com>
2025-07-01 10:01:13 +0900

    chore: eslint 설정 변경
    function style 변경 => 기존 arrow function 금지에서 허용으로 변경
    콜백 함수는 arrow function 사용 권장
    빈 함수 금지
    사용되지 않는 함수 인자나 변수 금지
    async 함수에서 반드시 await 사용

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -25,8 +25,23 @@ const eslintConfig = [
       // PascalCase: 클래스/컴포넌트
       'new-cap': ['error', { newIsCap: true, capIsNew: false }],
 
-      // 함수는 function 선언만 허용, 화살표 함수 금지
-      'func-style': ['error', 'declaration', { allowArrowFunctions: false }],
+      // 함수는 fucntion 또는 arrow function 모두 허용
+      'func-style': ['error', 'declaration', { allowArrowFunctions: true }],
+
+      // 콜백 함수는 arrow function 사용 권장
+      'prefer-arrow-callback': ['warn'],
+
+      // 빈 함수 금지 (테스트 목적일 경우는 예외 처리 가능)
+      'no-empty-function': ['warn'],
+
+      // 사용되지 않는 함수 인자나 변수 금지
+      'no-unused-vars': [
+        'warn',
+        { args: 'after-used', ignoreRestSiblings: true },
+      ],
+
+      // async 함수에서 반드시 await를 사용해야 함
+      'require-await': ['warn'],
     },
   }),
 
