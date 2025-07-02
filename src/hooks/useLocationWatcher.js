1e2d7e062a841e2c73ade9e63df5d8b1734797dc
rktlskan021 <rktlskan021@gmail.com>
2025-07-02 16:01:40 +0900

    feat: 플랫폼별 위치 추적 훅 구현
    Capacitor와 Web 환경에 따라 위치 추적 방식 분기
    위치 정보는 전역 상태를 통해 관리
    앱에서는 @capacitor/geolocation API 사용

---
+++ src/hooks/useLocationWatcher.js
@@ -0,0 +1,69 @@
+'use client';
+
+import { useEffect } from 'react';
+import { Geolocation } from '@capacitor/geolocation';
+import { Capacitor } from '@capacitor/core';
+import { useLocationStore } from '@/stores/useLocationStore';
+
+export default function useLocationWatcher() {
+  const setLocation = useLocationStore((state) => state.setLocation);
+
+  useEffect(() => {
+    let watchId;
+    let errorCount = 0;
+    if (Capacitor.getPlatform() === 'web') {
+      const startWatching = () => {
+        watchId = navigator.geolocation.watchPosition(
+          (pos) => {
+            const newCoords = {
+              lat: pos.coords.latitude,
+              lng: pos.coords.longitude,
+            };
+            setLocation(newCoords);
+          },
+          (err) => {
+            console.error(err.message);
+          },
+          { enableHighAccuracy: true, timeout: 10000, maximumAge: 0 }
+        );
+      };
+      startWatching();
+    } else {
+      const startWatching = async () => {
+        await Geolocation.requestPermissions();
+
+        watchId = Geolocation.watchPosition(
+          {
+            enableHighAccuracy: true, //최대한 정확한 위치 요구
+            timeout: 10000, // 10초 넘게 기다리면 실패로 간주
+            maximumAge: 0, // 이전에 캐싱된 위지 정보를 얼마 동안까지 허용할 것인지
+          },
+          (pos, err) => {
+            if (err) {
+              errorCount++;
+              if (errorCount >= 3) {
+                Geolocation.clearWatch({ id: watchId });
+              }
+              console.error('위치 추적 에러', err);
+              return;
+            }
+
+            if (pos) {
+              const newCoords = {
+                lat: pos.coords.latitude,
+                lng: pos.coords.longitude,
+              };
+              setLocation(newCoords);
+            }
+          }
+        );
+      };
+      startWatching();
+    }
+
+    return () => {
+      if (watchId) Geolocation.clearWatch({ id: watchId });
+      navigator.geolocation.clearWatch(watchId);
+    };
+  }, [setLocation]);
+}
