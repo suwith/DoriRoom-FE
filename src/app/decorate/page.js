00b9943ad766f3d9651ddad3dcb6cc0cf5759bb5
rktlskan021 <rktlskan021@gmail.com>
2025-07-18 15:44:05 +0900

    feat: 꾸미기 페이지 구현 및 라우팅

---
+++ src/app/decorate/page.js
@@ -0,0 +1,9 @@
+import HeaderNavigationBar from '../_components/HeaderNavigationBar';
+
+export default function Decorate() {
+  return (
+    <div className="flex justify-center items-center h-full">
+      <HeaderNavigationBar title="꾸미기" />
+    </div>
+  );
+}
