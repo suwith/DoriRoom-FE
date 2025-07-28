2dad359fe65541b9fe4276e356086972ef5958b1
rktlskan021 <rktlskan021@gmail.com>
2025-07-28 18:47:15 +0900

    feat: 상단 탭 UI 구현

--- src/app/collection/page.js
+++ src/app/collection/page.js
@@ -1,7 +1,40 @@
+'use client';
+
+import { useState } from 'react';
+
 export default function Collection() {
+  const [activeTab, setActiveTab] = useState(1);
+
   return (
-    <div className="flex justify-center items-center h-full">
-      <h1 className="text-xl font-bold text-black">도감</h1>
+    <div className="pb-24 max-w-[390px] mx-auto">
+      <div className="flex justify-around border-b border-neutral-200 mt-[60px]">
+        <div
+          className={`flex flex-col items-center justify-end cursor-pointer px-4 text-base ${
+            activeTab === 1
+              ? 'text-main-100 font-bold'
+              : 'text-neutral-300 font-semibold'
+          }`}
+          onClick={() => setActiveTab(1)}
+        >
+          지역과제
+          <hr
+            className={`transition-all duration-500 ease-out ${activeTab === 1 ? 'w-[80%]' : 'w-[0px]'}`}
+          />
+        </div>
+        <div
+          className={`flex flex-col items-center justify-end cursor-pointer px-4 text-base font-semibold ${
+            activeTab === 2
+              ? 'text-main-100 font-bold'
+              : 'text-neutral-300 font-semibold'
+          }`}
+          onClick={() => setActiveTab(2)}
+        >
+          일반과제
+          <hr
+            className={`transition-all duration-500 ease-out ${activeTab === 2 ? 'w-[80%]' : 'w-[0px]'}`}
+          />
+        </div>
+      </div>
     </div>
   );
 }
