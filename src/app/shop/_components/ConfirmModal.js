89cb80190076cff23c251807a50716bd1cf0f3ce
rktlskan021 <rktlskan021@gmail.com>
2025-07-29 21:24:13 +0900

    design: Confirm Modal width 수정

--- src/app/shop/_components/ConfirmModal.js
+++ src/app/shop/_components/ConfirmModal.js
@@ -13,7 +13,7 @@ export default function ConfirmModal({
 
   return (
     <div className="fixed inset-0 z-100 flex items-center justify-center bg-black/30">
-      <div className="flex flex-col gap-4 bg-white rounded-lg shadow-lg w-[25%] max-w-sm px-10 py-6 animate-fade-in">
+      <div className="flex flex-col gap-4 bg-white rounded-lg shadow-lg w-[80%] max-w-sm px-10 py-6 animate-fade-in">
         <p className="text-center text-gray-800 text-lg font-medium">
           {price} 도깨비불로 {name}을(를) 구매하시겠습니까?
         </p>
