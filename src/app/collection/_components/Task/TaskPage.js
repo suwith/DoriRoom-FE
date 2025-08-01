9f658a4012c416f61f5c08189de381899272fa1b
rktlskan021 <rktlskan021@gmail.com>
2025-08-01 17:17:49 +0900

    feat: 과제 필터링 기능 추가

--- src/app/collection/_components/Task/TaskPage.js
+++ src/app/collection/_components/Task/TaskPage.js
@@ -1,6 +1,7 @@
 'use client';
+
 import TaskList from './TaskList';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { IoIosArrowDown } from 'react-icons/io';
 import { FaXmark } from 'react-icons/fa6';
 import { IoCheckmarkSharp } from 'react-icons/io5';
@@ -12,9 +13,166 @@ const filterList = [
   { id: 3, name: '달성 과제' },
 ];
 
+// 예시 데이터
+const tmpTasks = [
+  {
+    id: 1,
+    title: '축제 즐겨찾기 10개 하기',
+    point: 100,
+    reward: 5,
+    progress: 70,
+    score: 7,
+    complete: 10,
+    status: '시작',
+  },
+  {
+    id: 2,
+    title: '일기 좋아요 10개 누르기',
+    point: 100,
+    reward: 10,
+    progress: 70,
+    score: 7,
+    complete: 10,
+    status: '시작',
+  },
+  {
+    id: 3,
+    title: '이웃 5명 달성하기',
+    point: 200,
+    reward: 10,
+    progress: 0,
+    score: 0,
+    complete: 10,
+    status: '대기',
+  },
+  {
+    id: 4,
+    title: '내 방 촬영하기 1회',
+    point: 100,
+    reward: 5,
+    progress: 100,
+    score: 1,
+    complete: 1,
+    status: '달성',
+  },
+  {
+    id: 5,
+    title: '축제 즐겨찾기 10개 하기',
+    point: 100,
+    reward: 5,
+    progress: 70,
+    score: 7,
+    complete: 10,
+    status: '시작',
+  },
+  {
+    id: 6,
+    title: '일기 좋아요 10개 누르기',
+    point: 100,
+    reward: 10,
+    progress: 70,
+    score: 7,
+    complete: 10,
+    status: '시작',
+  },
+  {
+    id: 7,
+    title: '이웃 5명 달성하기',
+    point: 200,
+    reward: 10,
+    progress: 0,
+    score: 0,
+    complete: 10,
+    status: '대기',
+  },
+  {
+    id: 8,
+    title: '내 방 촬영하기 1회',
+    point: 100,
+    reward: 5,
+    progress: 100,
+    score: 1,
+    complete: 1,
+    status: '달성',
+  },
+  {
+    id: 9,
+    title: '축제 즐겨찾기 10개 하기',
+    point: 100,
+    reward: 5,
+    progress: 70,
+    score: 7,
+    complete: 10,
+    status: '시작',
+  },
+  {
+    id: 10,
+    title: '일기 좋아요 10개 누르기',
+    point: 100,
+    reward: 10,
+    progress: 70,
+    score: 7,
+    complete: 10,
+    status: '시작',
+  },
+  {
+    id: 11,
+    title: '이웃 5명 달성하기',
+    point: 200,
+    reward: 10,
+    progress: 0,
+    score: 0,
+    complete: 10,
+    status: '대기',
+  },
+  {
+    id: 12,
+    title: '내 방 촬영하기 1회',
+    point: 100,
+    reward: 5,
+    progress: 100,
+    score: 1,
+    complete: 1,
+    status: '달성',
+  },
+];
+
 export default function TasksPage() {
   const [selectFilter, setSelectFilter] = useState(filterList[0]);
   const [bottomSheetOpen, setBottomSheetOpen] = useState(false);
+  const [tasks, setTasks] = useState(tmpTasks);
+
+  useEffect(() => {
+    console.log(tasks);
+    switch (selectFilter.id) {
+      case 0:
+        setTasks(tmpTasks); // 전체보기: 모든 과제
+        return;
+      case 1:
+        setTasks(
+          tmpTasks.filter(
+            (task) => selectFilter.id === 1 && task.status === '대기'
+          )
+        );
+        return;
+      case 2:
+        setTasks(
+          tmpTasks.filter(
+            (task) => selectFilter.id === 2 && task.status === '시작'
+          )
+        );
+        return;
+      case 3:
+        setTasks(
+          tmpTasks.filter(
+            (task) => selectFilter.id === 3 && task.status === '달성'
+          )
+        );
+        return;
+      default:
+        setTasks(tmpTasks); // 기본값 (전체보기)
+    }
+  }, [selectFilter]);
 
   return (
     <div className="space-y-3 p-4">
@@ -25,7 +183,7 @@ export default function TasksPage() {
         <span>{selectFilter.name}</span>
         <IoIosArrowDown />
       </div>
-      <TaskList type="general" />
+      <TaskList type="general" tasks={tasks} />
       <div
         className={`fixed bottom-0 left-1/2 transform -translate-x-1/2 w-full max-w-[390px] mx-auto pb-10 z-100 bg-white rounded-t-xl px-3 pt-4 shadow-[0_-4px_12px_rgba(0,0,0,0.1)] transition-transform duration-300 ease-in-out ${bottomSheetOpen ? 'translate-y-0' : 'translate-y-full'}`}
       >
