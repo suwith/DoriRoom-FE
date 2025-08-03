f195923f93ed75378b8f0bc35d21d183127a58d7
rktlskan021 <rktlskan021@gmail.com>
2025-08-03 23:42:40 +0900

    feat: 일반 과제, 지역 과제 분기 처리

--- src/app/collection/_components/Task/TaskList.js
+++ src/app/collection/_components/Task/TaskList.js
@@ -1,11 +1,14 @@
 import TaskCard from './TaskCard';
+import ResionTaskCard from './ResionTaskCard';
 
 export default function TaskList({ type, regionId, tasks }) {
   return (
     <div className="space-y-3 mb-16">
-      {tasks.map((task) => (
-        <TaskCard key={task.id} {...task} />
-      ))}
+      {tasks.map((task) => {
+        if (type === 'general') return <TaskCard key={task.id} {...task} />;
+        else if (type === 'resion')
+          return <ResionTaskCard key={task.id} {...task} />;
+      })}
     </div>
   );
 }
