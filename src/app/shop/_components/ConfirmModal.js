6b152b8f1496b149e3c8dc325bd0413b6afbf719
suwith <suwith@kookmin.ac.kr>
2025-08-05 15:33:14 +0900

    design: 상점 구매 모달창 디자인 수정 Dori-Room#11

--- src/app/shop/_components/ConfirmModal.js
+++ src/app/shop/_components/ConfirmModal.js
@@ -12,13 +12,14 @@ export default function ConfirmModal({
   if (!isOpen) return null;
 
   return (
-    <div className="fixed inset-0 z-100 flex items-center justify-center bg-black/30">
-      <div className="flex flex-col gap-4 bg-white rounded-lg shadow-lg w-[80%] max-w-sm px-10 py-6 animate-fade-in">
-        <p className="text-center text-gray-800 text-lg font-medium">
+    <div className="fixed inset-0 z-50 flex justify-center">
+      <div className="w-full max-w-[390px] bg-black/30 px-4 flex items-center justify-center">
+        <div className="flex flex-col gap-4 bg-white rounded-lg shadow-lg w-[90%] max-w-sm px-10 py-6 animate-fade-in">
+        <p className="text-center text-gray-800 text-base font-semibold">
           {price} 도깨비불로 {name}을(를) 구매하시겠습니까?
         </p>
-        <p className="text-center text-neutral-500 font-regular text-[14px]">
-          남은 잎사귀 {credit}개
+        <p className="text-center text-neutral-500 font-regular text-[13px] mb-2 ">
+          남은 도깨비불 : {credit-price}개
         </p>
         <div className="flex justify-between gap-3">
           <button
@@ -33,6 +34,7 @@ export default function ConfirmModal({
           >
             아니요
           </button>
+        </div>
         </div>
       </div>
     </div>
