e99ab51807fa8c12091b87b44a7d11e4cf119356
rktlskan021 <rktlskan021@gmail.com>
2025-08-10 19:24:01 +0900

    feat: 점수 확인 및 리셋 코드 적용

--- src/app/collection/_components/Quiz/Result.js
+++ src/app/collection/_components/Quiz/Result.js
@@ -1,14 +1,19 @@
+'use client';
+
 import Link from 'next/link';
+import { useQuizStore } from '@/stores/useQuizStore';
 
 export default function Result({ quiz }) {
   const { exp, credit } = quiz.reward;
+  const score = useQuizStore((s) => s.score);
+  const reset = useQuizStore((s) => s.reset);
   return (
     <div className="max-w-[390px] w-screen h-screen bg-linear-to-t from-main-100/15 to-background">
       <div className="h-screen relative mx-[16px]">
         <div className="absolute top-3/7 left-1/2 -translate-x-1/2 -translate-y-1/2 text-center w-full">
           <p className="font-medium text-2xl text-neutral-900">축하드려요!</p>
           <p className="font-semibold text-2xl text-neutral-900">
-            모든 퀴즈를 맞히셨어요 🎉
+            {score} 개를 맞추셨어요!
           </p>
           <p className="mt-3 font-normal text-base text-main-100">
             보상으로 {exp}xp와 {credit} 도깨비불이 지급돼요.
@@ -22,8 +27,9 @@ export default function Result({ quiz }) {
         <Link
           className="absolute bottom-10 bg-main-100 text-background text-center text-xl font-semibold rounded-md w-full py-2.5 justify-self-end"
           href={`/collection/`}
+          onClick={() => reset()}
         >
-          퀴즈 시작하기
+          보상 받기
         </Link>
       </div>
     </div>
