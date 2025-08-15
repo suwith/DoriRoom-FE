ecd24901686ca83d41ba1bbca4783c64ff4ab097
suwith <suwith@kookmin.ac.kr>
2025-08-16 01:31:27 +0900

    chore: 프록시 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  trailingSlash: true,
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://43.202.194.181:8080/api/:path*', // 백엔드 HTTP 그대로
+      },
+    ];
+  },
 };
 
 export default nextConfig;
