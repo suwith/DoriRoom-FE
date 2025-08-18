26e8ba329cb64aff251011906807c6f9e5c9d7ec
rktlskan021 <rktlskan021@gmail.com>
2025-08-18 17:23:32 +0900

    chore: 카카오맵 관련 불필요 파일 제거

--- src/app/map/_components/use-kakao-loader.js
+++
@@ -1,8 +0,0 @@
-import { useKakaoLoader as useKakaoLoaderOrigin } from 'react-kakao-maps-sdk';
-
-export default function useKakaoLoader() {
-  useKakaoLoaderOrigin({
-    appkey: process.env.KAKAOJSKEY,
-    libraries: ['clusterer', 'drawing', 'services'],
-  });
-}
