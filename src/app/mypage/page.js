a97385f46d3bf8b947f7518492476fca2419587f
rktlskan021 <rktlskan021@gmail.com>
2025-08-26 17:05:36 +0900

    feat: 하단 마이페이지 목록 구현

--- src/app/mypage/page.js
+++ src/app/mypage/page.js
@@ -1,4 +1,5 @@
 import UserProfile from './_components/UserProfile';
+import Link from 'next/link';
 
 export default function Mypage() {
   return (
@@ -6,7 +7,39 @@ export default function Mypage() {
       <div className="flex-2 flex items-center justify-center bg-main-5 px-4">
         <UserProfile />
       </div>
-      <div className="flex-3 px-4"></div>
+      <div className="flex-3 flex flex-col divide-y-2 divide-neutral-100">
+        <IconButton
+          icon={<i className="mgc_pin_fill text-main-100 text-xl" />}
+          label="공지사항"
+          textColor="text-neutral-900"
+        />
+        <IconButton
+          icon={<i className="mgc_pen_fill text-main-100 text-xl" />}
+          label="이용약관"
+          textColor="text-neutral-900"
+        />
+        <IconButton
+          icon={<i className="mgc_entrance_fill text-main-100 text-xl" />}
+          label="로그아웃"
+          textColor="text-neutral-900"
+        />
+        <IconButton
+          icon={<i className="mgc_delete_2_fill text-main-100 text-xl" />}
+          label="탈퇴하기"
+          textColor="text-neutral-900"
+        />
+      </div>
     </div>
+  );
+}
+
+function IconButton({ icon, label, href = '/', textColor }) {
+  return (
+    <Link href={href}>
+      <div className="flex items-center space-x-2 py-3 px-4">
+        {icon}
+        <span className={`text-sm font-normal ${textColor}`}>{label}</span>
+      </div>
+    </Link>
   );
 }
