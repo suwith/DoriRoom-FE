99e7448fe56dd02b8e0c03266bd0879e67057e8a
rktlskan021 <rktlskan021@gmail.com>
2025-09-01 01:46:28 +0900

    feat: Home 페이지 UI 구현

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,34 @@
+'use client';
+
 import HeaderBar from './home/Header';
+import RoomStatsCard from './_components/RoomStatsCard';
+import useMyRoom from '@/hooks/user/useMyRoom';
+import LoadingContent from './_components/LoadingContent';
 
 export default function Home() {
+  const { data, loading, error } = useMyRoom();
+
+  if (loading)
+    return (
+      <LoadingContent
+        loading={loading}
+        className="max-w-[390px] w-screen h-screen"
+      />
+    );
+
   return (
     <>
-      <HeaderBar />
+      <HeaderBar credit={data.credit} />
 
-      <div className="h-full flex justify-center items-center p-4 max-w-[390px] w-screen">
+      <div className="flex-1 h-full flex justify-center items-center p-4 max-w-[390px] w-screen">
         <p className="text-main">캐릭터 위치</p>
       </div>
+
+      <RoomStatsCard
+        today={data.viewCount}
+        like={data.likeCount}
+        className="fixed bottom-22"
+      />
     </>
   );
 }
