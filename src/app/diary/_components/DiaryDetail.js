68ff84e017bdb66c58db62bd0f210dc691d4f03d
suwith <suwith@kookmin.ac.kr>
2025-09-04 16:21:59 +0900

    design: 일기 작성자 정보 추가 Dori-Room#34

--- src/app/diary/_components/DiaryDetail.js
+++ src/app/diary/_components/DiaryDetail.js
@@ -8,11 +8,16 @@ import TwoButtonModal from '@/app/_components/TwoButtonModal';
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 import useDiaryLike from '@/hooks/diary/useDiaryLike';
+import { useAuthStore } from '@/stores/useAuthStore';
 
 export default function DiaryDetail({ diary }) {
   const router = useRouter();
   const [isBottomOpen, setIsBottomOpen] = useState(true);
   const [showDeleteModal, setShowDeleteModal] = useState(false);
+
+  const user = useAuthStore((state) => state.user);
+
+  const isMine = diary.author.id === user.id;
 
   // 좋아요 훅 사용
   const {
@@ -33,7 +38,7 @@ export default function DiaryDetail({ diary }) {
   };
 
   return (
-    <div className="pt-20 pb-28">
+    <div className="pt-20 pb-60">
       <HeaderNavigationBar
         title={diary.festival?.title || '연결된 축제'}
         type="diary"
@@ -43,6 +48,29 @@ export default function DiaryDetail({ diary }) {
       />
 
       <div className="p-5 space-y-5 whitespace-pre-line">
+        <div className="flex items-center justify-between">
+          <div className="flex items-center gap-2">
+            <img
+              src={diary.profileImage || '/images/profileImage_default.svg'}
+              alt="profile"
+              className="w-8 h-8 rounded-full oobject-cover"
+            />
+            <div className="text-sm font-semibold text-neutral-800">
+              {diary.author.name}
+            </div>
+            {isMine && (
+              <div className="text-[10px] bg-sub2-5 text-sub2-100 rounded-sm px-1 flex items-center justify-center">
+                내 리뷰
+              </div>
+            )}
+          </div>
+
+          <div className="flex items-center gap-2">
+            <button className="text-[11px] px-3 py-0.5 bg-main-5 text-main-100 rounded">
+              방문
+            </button>
+          </div>
+        </div>
         <div className="text-sm text-neutral-800">{diary.content}</div>
         {diary.images?.length > 0 && (
           <div className="flex overflow-x-scroll gap-2 scrollbar-hide">
