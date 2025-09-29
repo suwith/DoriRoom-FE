8d429be8b46e1c9f0f97e1561fc61619e0f0018a
rktlskan021 <rktlskan021@gmail.com>
2025-09-29 15:16:57 +0900

    feat: 말풍선 API 적용

--- src/app/home/edit-intro/page.js
+++ src/app/home/edit-intro/page.js
@@ -1,13 +1,35 @@
 'use client';
 
 import EditHeaderNavBar from '@/app/mypage/_components/EditHeaderNavBar';
-import { useState } from 'react';
+import usePutSpeech from '@/hooks/user/usePutSpeech';
+import LoadingContent from '@/app/_components/LoadingContent';
+import { useToast } from '@/app/_providers/ToastProvider';
+import { useEffect, useState } from 'react';
 
 export default function page() {
   const [context, setContext] = useState('');
+  const { mutate, data, loading, error } = usePutSpeech();
+  const { show } = useToast();
+
+  useEffect(() => {
+    if (data?.statusCode === 400) show(data?.error);
+    else if (data?.statusCode === 200)
+      show({
+        message: '한줄소개가 정상적으로 등록되었어요!',
+        variant: 'success',
+      });
+  }, [data]);
+
+  if (loading) return <LoadingContent loading={loading} />;
+
   return (
     <div className="w-screen h-screen px-4 pt-28">
-      <EditHeaderNavBar title="한줄소개" />
+      <EditHeaderNavBar
+        title="한줄소개"
+        onClick={async () => {
+          await mutate({ speechBubble: context });
+        }}
+      />
       <div className="space-y-4">
         <p className="font-semibold text-base">한줄소개를 입력해 주세요.</p>
         <div className="bg-neutral-100 rounded-lg p-4 font-medium">
